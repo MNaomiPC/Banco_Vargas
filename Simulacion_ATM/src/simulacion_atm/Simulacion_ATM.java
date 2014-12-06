7d5df53efe8b6a57fd73a9e03d70b5a81196224e
Luis Angel Perez Muñoz <ingluisperez.m@outlook.com>
2014-12-05 23:46:39 -0600

    Vista
    se agregó la tabla de forma visual
    
    falta mas diseño

--- Simulacion_ATM/src/simulacion_atm/Simulacion_ATM.java
+++ Simulacion_ATM/src/simulacion_atm/Simulacion_ATM.java
@@ -6,6 +6,9 @@
 package simulacion_atm;
 
 import java.util.LinkedList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 
 /**
  *
@@ -17,6 +20,19 @@ public class Simulacion_ATM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        JFrame ventana=new JFrame("Simulacion");
+        ventana.setSize(900,600);
+        ventana.setLocationRelativeTo(null);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panelPrincipal=new JPanel();
+        ventana.setResizable(false);
+        
+        String []Columnas={"Cliente","r","Tiempo entre llegada","Reloj","E?","Contador","Tiempo"
+                + " Inicio de Servicio","r","Tiempo entre Servicio","Tiempo fin de Servicio","Tiempo de Permanencia"
+                + " en el sistema"};
+        String [][]Data =new String[5][11];
+        
+        
         LinkedList fila = new LinkedList();
         Cliente anterior = null;
         double reloj = 0;
@@ -49,8 +65,30 @@ public class Simulacion_ATM {
             ((Cliente) fila.get(fila.size() - 1)).calcularTiempoFinServicio();
             ((Cliente) fila.get(fila.size() - 1)).calcularTiempoPermanencia();
             Cliente actual = (Cliente) fila.get(fila.size() - 1);
-            System.out.println(i + 1 + "   " + actual.getrLlegada() + "   " + actual.gettLlegada() + "   " + actual.getReloj() + "   " + actual.isEspero() + "   " + actual.getCont() + "   " + actual.gettInicioServicio() + "   " + actual.getrServicio() + "   " + actual.gettServicio() + "   " + actual.gettFinServicio() + "   " + actual.gettPermanecia());
+            
+            Data[i][0]=(i+1)+"";
+            Data[i][1]=actual.getrLlegada()+"";
+            Data[i][2]=actual.gettLlegada()+"";
+            Data[i][3]=actual.getReloj()+"";
+            Data[i][4]=actual.isEspero()+"";
+            Data[i][5]=actual.getCont()+"";
+            Data[i][6]=actual.gettInicioServicio()+"";
+            Data[i][7]=actual.getrServicio()+"";
+            Data[i][8]=actual.gettServicio()+"";
+            Data[i][9]=actual.gettFinServicio()+"";
+            Data[i][10]=actual.gettPermanecia()+"";
+            
+//            System.out.println(i + 1 + "   " + actual.getrLlegada() + "   " + actual.gettLlegada() + " "
+//                    + "  " + actual.getReloj() + "   " + actual.isEspero() + "  "
+//                    + " " + actual.getCont() + "   " + actual.gettInicioServicio() + " "
+//                    + "  " + actual.getrServicio() + "   " + actual.gettServicio() + "   "
+//                    + "" + actual.gettFinServicio() + "   " + actual.gettPermanecia());
         }
+        JTable t=new JTable(Data,Columnas);
+        
+        panelPrincipal.add(t);
+        ventana.add(panelPrincipal);
+        ventana.setVisible(true);
     }
 
 }
